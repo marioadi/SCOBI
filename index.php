eeb0a5cedf5f6881b138d520de831da5d3475f1d
MarioDevNote <mariojunioradi@gmail.com>
2019-04-24 23:07:39 -0300

    BootStrap adicionado ao projeto, implementação do Header

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SCOBI</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="header">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12">
+					<div class="col-xs-12 col-sm-6">
+						<img class="pull-left" src="img/Logo-AST.png" width="50" height="50" />
+					</div>
+					<div class="col-xs-12 col-sm-6">
+						<p class="text-right">SCOBI</p>
+						<small class="pull-right">Sistema de Gerenciamento de bibliotecas</small>
+					</div>
+				</div>		
+			</div>
+		</div>
+	</div>	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
