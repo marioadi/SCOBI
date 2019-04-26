c3719433201cd895a77aeed7053733a772f30318
MarioDevNote <mariojunioradi@gmail.com>
2019-04-25 23:56:58 -0300

    Adicionado o campo de busca, footer e inicio a utilização da tabela de dados

--- index.php
+++ index.php
@@ -7,8 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
+	<!-- Inicio Header -->
 	<div class="header">
 		<div class="container-fluid">
 			<div class="row">
@@ -23,7 +25,70 @@
 				</div>		
 			</div>
 		</div>
-	</div>	
+	</div>
+	<!-- Fim Header -->
+	<!-- Inicio Busca -->
+	<div class="container mar-top-40 mar-bot-40">
+		<div class="row">
+	        <div class="col-sm-6 col-sm-offset-3">
+	            <div id="custom-search-input">
+	                <div class="input-group col-md-12">
+	                    <input type="text" class="form-control" placeholder="Buscar" />
+	                    <span class="input-group-btn">
+	                        <button class="btn" type="button">
+	                            <i class="glyphicon glyphicon-search"></i>
+	                        </button>
+	                    </span>
+	                </div>
+	            </div>
+	        </div>
+		</div>
+	</div>
+	<!-- Fim Busca -->
+	<!-- Inicio Tabela -->
+	<div class="container">
+		<table class="table">
+			<thead>
+				<tr>
+					<th> Autor </th>
+					<th> Edição</th>
+					<th> Emprestado </th>
+					<th> Titulo</th>
+					<th> Categoria </th>
+				</tr>	
+			</thead>
+			<tbody>
+				<tr>
+					<td> Mario Jr </td>
+					<td> 2° </td>
+					<td> SIM </td>
+					<td> O piveta </td>
+					<td> Zoool </td>
+				</tr>
+				<tr>
+					<td> Mario Jr </td>
+					<td> 2° </td>
+					<td> SIM </td>
+					<td> O piveta </td>
+					<td> Zoool </td>	
+				</tr>
+				<tr>
+					<td> Mario Jr </td>
+					<td> 2° </td>
+					<td> SIM </td>
+					<td> O piveta </td>
+					<td> Zoool </td>	
+				</tr>
+				
+			</tbody>
+		</table>
+	</div>
+	<!-- Fim Tabela -->
+	<!-- Inicio Footer -->
+	<div class="footer">
+		<p id="text-footer" class="text-center"> AST Solutions &copy; - <small> MarioDev</small></p>
+	</div>
+	<!-- Fim Footer -->
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
